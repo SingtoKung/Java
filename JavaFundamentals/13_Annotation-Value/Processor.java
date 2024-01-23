3fdceedc3243e5b1f24649c41b5ffb74110b6e44
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-23 23:32:01 +0700

    Lesson 12-13: Annotation

---
+++ JavaFundamentals/13_Annotation-Value/Processor.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Method;
+
+public class Processor {
+    public static void processAnnotation(Object target) {
+        Class<?> clazz = target.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            GetMapping getMapping = method.getAnnotation(GetMapping.class);
+            if (getMapping != null) {
+                System.out.println("Do sth. here - GetMapping: " + getMapping.value());
+            }
+        }
+    }
+}
