81f47537a04b06e9270b60fe5576e2c982224989
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-23 14:40:36 +0700

    Lesson 1: Starting java

---
+++ JavaFundamentals/01_Starting-Point/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // type name = value;
+        String msg = "Hello World!";
+        System.out.println(msg);
+
+        int loadTerm = 36;
+        System.out.println(number);
+
+        float interestRate = 7.5f;
+        System.out.println(interestRate);
+
+        double loadAmount = 10000.0d;
+        System.out.println(loadAmount);
+
+        boolean isApproved = true;
+        System.out.println(isApproved);
+    }
+}
