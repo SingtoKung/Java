8c6c3810676ffcb5cd95f64d9822eb5a2a17d56e
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-28 15:06:40 +0700

    Java beginner - Constant

---
+++ JavaBeginner/02_Constant/Constant.java
@@ -0,0 +1,10 @@
+public class Constant {
+    public static void main(String[] args) {
+        // Keyword: final
+        // Variable name: All uppercase
+
+        final float NUM1 = 10;
+
+        System.out.println(NUM1);
+    }
+}
