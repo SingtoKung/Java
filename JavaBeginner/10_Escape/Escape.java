be454b9e1eb14e298f5b17da5f82353993376115
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-28 18:01:40 +0700

    Java beginner - Escape character

---
+++ JavaBeginner/10_Escape/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main(String[] args) {
+        /*
+            \n => ขึ้นบรรทัดใหม่
+            \t => tab
+        */
+
+        System.out.println("Hello\tworld");
+        System.out.println("Hi\nthere");
+    }
+}
