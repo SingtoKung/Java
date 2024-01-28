8c7c2a2214d7d807f994074b7baaf629fabc7040
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-28 15:29:39 +0700

    Java beginner - Max/Min

---
+++ JavaBeginner/04_Max-Min/MaxMin.java
@@ -0,0 +1,12 @@
+public class MaxMin {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
