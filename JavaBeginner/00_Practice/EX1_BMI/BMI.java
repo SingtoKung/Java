2d1366cedcf9c9a599109c66accfa9528995e6c2
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-29 15:37:38 +0700

    Java beginner - Modified BMI cal.

--- JavaBeginner/00_Practice/EX1_BMI/BMI.java
+++ JavaBeginner/00_Practice/EX1_BMI/BMI.java
@@ -15,5 +15,26 @@ public class BMI {
         System.out.println("--------------------------");
         System.out.print("Your BMI is ");
         System.out.printf("%,.2f", BMI);
+
+        String result;
+        if (BMI <= 16) {
+            result = "Severe Thinness";
+        } else if (BMI <= 17) {
+            result = "Moderate Thinness";
+        } else if (BMI <= 18.5) {
+            result = "Mild Thinness";
+        } else if (BMI <= 25) {
+            result = "Normal";
+        } else if (BMI <= 30) {
+            result = "Overweight";
+        } else if (BMI <= 35) {
+            result = "Obese Class I";
+        } else if (BMI <= 40) {
+            result = "Obese Class II";
+        } else {
+            result = "Obese Class III";
+        }
+        System.out.println();
+        System.out.println("BMI classification: " + result);
     }
 }
