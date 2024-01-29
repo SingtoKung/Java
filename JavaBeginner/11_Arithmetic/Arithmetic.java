eb191a53089037c5d259a1c748c87d1bfa949d90
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-29 14:15:09 +0700

    Java beginner - Arithmetic operator

---
+++ JavaBeginner/11_Arithmetic/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Total = " + (a - b));
+        System.out.println("Total = " + (a * b));
+        System.out.println("Total = " + (a / b));
+        System.out.println("Mod = " + (a % b));
+    }
+}
