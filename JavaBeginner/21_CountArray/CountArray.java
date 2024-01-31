fbe224a00b6506492a309762986454613b6c2279
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-31 10:46:19 +0700

    Java beginner - Count array

---
+++ JavaBeginner/21_CountArray/CountArray.java
@@ -0,0 +1,12 @@
+public class CountArray {
+    public static void main(String[] args) {
+        int[] number = {1,2,3,4,5,6};
+        int size = number.length;
+        boolean[] status = {true,false,true};
+        String[] name = {"What", "Where", "When", "Why"};
+
+        System.out.println(size);
+        System.out.println(status.length);
+        System.out.println(name.length);
+    }
+}
