0411cd7b41a8cc20fb10655b52b29148500dd049
insuwathaiwan <insuwathaiwan@central.co.th>
2024-01-31 10:40:16 +0700

    Java beginner - Basic array

---
+++ JavaBeginner/20_Array/ArrayBasic.java
@@ -0,0 +1,14 @@
+public class ArrayBasic {
+    public static void main(String[] args) {
+        
+        int[] number = new int[4];
+        number[0] = 10;
+        number[1] = 20;
+        number[2] = 40;
+        number[3] = 50;
+        System.out.println(number[0]);
+
+        int[] number2 = {10, 20, 30};
+        System.out.println(number2[1]);
+    }
+}
