e9326138fe436142cf7c36df580bd3ea57c0f5be
insuwathaiwan <insuwathaiwan@central.co.th>
2024-02-04 14:51:40 +0700

    Java OOP - Final class

---
+++ JavaOOP/05_Final-Class/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Programmer p1 = new Programmer("Singto Kung", 24000.0d);
+        // Accounting a1 = new Accounting("Suwa In", 27000.0d);
+        
+        // System.out.println(Company.name = "Hey");
+        System.out.println(Company.location);
+    
+    }
+}
